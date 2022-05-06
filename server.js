eb13450a382defe8295492bb85d584e03579d2e6
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-06 18:11:37 -0300

    04-Model and schema

--- server.js
+++ server.js
@@ -1,39 +1,24 @@
 const express = require('express');
 const app = express();
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb+srv://francis8976:YaTNPCXktKNJvR20@cluster0.xs9gz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoUri = 'mongodb+srv://francis8976:YaTNPCXktKNJvR20@cluster0.xs9gz.mongodb.net/myApp?retryWrites=true&w=majority';
 
-const client = new MongoClient(mongoUri);
+mongoose.connect(mongoUri);
 
+////////// SCHEMA ////////
 
-app.get('/api/users',async(req,res)=>{
-    try {
-        await client.connect();
-        const database = client.db('myApp');
-        const collection = database.collection('users');
-        const query = await collection.insertOne({
-            name:'Francis',
-            lastname:'Jones'
-        })
-        console.log(query)
-        res.status(200).json({awesome:'yes'})
-    } catch(error){
-        throw error
-    } finally {
-        await client.close();
-        console.log('all is done')
-    }
-})
+const carSchema = mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    avail: Boolean
+});
 
+const Car = mongoose.model('Car',carSchema)
 
+//////////////////////////
 
-// MongoClient.connect(mongoUri,(err, client)=>{
-//     if(err){
-//         throw err
-//     }
-//     console.log('Connected to the DB')
-// });
 
 
 const port = process.env.POST || 3001;
